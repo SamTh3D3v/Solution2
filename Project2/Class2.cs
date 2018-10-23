3ba1ef5c0432241788f429668a9e8c9028ce0895
oel <oel@NL-18MHCJ2>
2018-10-23 13:10:39 +0200

    Add project files.

---
+++ Project2/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    public class Class2
+    {
+        public void TestMethod()
+        {
+            //this method should be able to call the common project
+        }
+    }
+}
